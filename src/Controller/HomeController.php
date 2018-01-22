795209b15ddb76990d507bae92972a5cd35e70d3
Prog <koobetone@gmail.com>
2018-01-22 15:50:48 +0200

    all product, templates

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,16 +2,25 @@
 
 namespace App\Controller;
 
+use App\Entity\Product;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
-class HomeController
+class HomeController extends Controller
 {
     /**
      * @Route("/")
      */
-    public function home()
+    public function home(\Twig_Environment $twig)
     {
-        return new Response('Hello world!');
+        $products = $this->getDoctrine()->getRepository(Product::class)->findAll();
+        if (0 === count($products)) {
+            die('error');
+        }
+        $content = $twig->render('product/index.html.twig', ['products' => $products]);
+        $response = new Response();
+        $response->setContent($content);
+        return $response;
     }
 }
